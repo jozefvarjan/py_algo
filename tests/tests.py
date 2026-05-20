6520e3e5014bda3a3b5a13fe78d1733c18ff865d
jvarjan <j.varjan@email.cz>
2026-05-20 22:10:56 +0200

    add pytes-check

--- tests/tests.py
+++ tests/tests.py
@@ -1,4 +1,6 @@
 import pytest
+import pytest_check as check
+
 
 import src.array_algs
 
@@ -15,9 +17,11 @@ def test_find_triplets_should_pass():
     assert src.array_algs.find_triplets_with_zero_sum(test_data_pass) == [[-1, -1, 2], [-1, 0, 1]]
 
 def test_find_sum_of_even_numbers():
-    assert src.array_algs.find_sum_of_even_numbers(14) == 56
-    assert src.array_algs.find_sum_of_even_numbers(16) == 72
-    assert src.array_algs.find_sum_of_even_numbers(114) == 3306
+    test_data: list[tuple[int, int]] = [
+        (14, 56), (16, 72), (114, 3306)
+    ]
+    for input, result in test_data:
+         check.equal(src.array_algs.find_sum_of_even_numbers(input), result)
 
 def test_find_sum_of_even_numbers_should_fail():
     with pytest.raises(ValueError):
